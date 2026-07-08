6dea78478f20c62af8130904ad1b6be7798e2f72
dinda raras <hrdatamedia2024@gmail.com>
2026-07-08 12:01:41 +0700

    perubahan lokasi

--- assets/js/config/supabase.js
+++ assets/js/config/supabase.js
@@ -1,6 +1,8 @@
-const SUPABASE_URL = "https://axehgewoxgzcjxrnoeix.supabase.co/rest/v1/";
+const SUPABASE_URL =
+"https://axehgewoxgzcjxrnoeix.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4ZWhnZXdveGd6Y2p4cm5vZWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMjcwMTMsImV4cCI6MjA5ODcwMzAxM30.SlNV5YqzMJ5nZ2Yj5YUSmzlWhtUzpgd3heOaby4zGPc";
+const SUPABASE_ANON_KEY =
+"sb_publishable_KnHZUpT_IUO72RYdlc6wdw_gMhQwieC";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
